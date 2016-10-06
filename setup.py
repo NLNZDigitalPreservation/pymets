a42ac3a804d1c4e55f486d719071239e99ae1977
Sean Mosely <sean.mosely@gmail.com>
2016-10-07 09:38:39 +1300

    removed tests from packages in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+VERSION="v0.1.0~git"
+
 config = {
 	'name':'pymets',
 	'version':'0.1.0',
 	'author':'Sean Mosely',
 	'author_email':'sean.mosely@gmail.com',
-	'packages':['pymets','pymets.tests'],
+	'packages':['pymets'],
 	'description':'Python library for building METS XML documents',
-	'install_requires':['lxml==3.6.4',]}
+	'install_requires':['lxml==3.6.4',],
+	'download_url': 'https://github.com/NLNZDigitalPreservation/pymets/archive/'+VERSION+'.tar.gz',}
 
 setup(**config)
