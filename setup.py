b142f9a879870cc540df44c5a44361709ae02166
Sean Mosely <sean.mosely@dia.govt.nz>
2018-01-17 14:36:52 -0800

    modified lxml requirement in setup.py from '==' to '>='

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
 	'author_email':'sean.mosely@gmail.com',
 	'packages':['pymets'],
 	'description':'Python library for building METS XML documents',
-	'install_requires':['lxml==3.6.4',],
+	'install_requires':['lxml>=3.6.4',],
 	'download_url': 'https://github.com/NLNZDigitalPreservation/pymets/archive/v'+VERSION+'.tar.gz',
 	'license': 'MIT',
 	}
